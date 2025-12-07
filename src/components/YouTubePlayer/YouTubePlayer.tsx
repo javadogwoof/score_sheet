5912bc3ab6fc8531dfe79fb96094a37b478a9489
Taiga Fujimura <taiga221.develop@gmail.com>
2025-12-07 17:24:08 +0900

    youtube動画を再生する

--- src/components/YouTubePlayer/YouTubePlayer.tsx
+++ src/components/YouTubePlayer/YouTubePlayer.tsx
@@ -1,25 +1,15 @@
-import styles from './YouTubePlayer.module.css'
+import styles from './YouTubePlayer.module.scss'
 
 export interface YouTubePlayerProps {
   videoId: string
 }
 
-export function YouTubePlayer(props: YouTubePlayerProps) {
-    const { videoId } = props
-
+export function YouTubePlayer({ videoId }: YouTubePlayerProps) {
   return (
-    <div className={styles.container}>
-        <div className={styles.playerWrapper}>
-            <iframe
-                width="560"
-                height="315"
-                src={`https://www.youtube.com/embed/${videoId}`}
-                title="YouTube video player" frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerPolicy="strict-origin-when-cross-origin"
-                allowFullScreen
-            ></iframe>
-        </div>
-    </div>
+    <iframe
+      className={styles.iframe}
+      src={`https://javadogwoof.github.io/score_sheet/?v=${videoId}`}
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    />
   )
 }
